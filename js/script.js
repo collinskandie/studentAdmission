fef2d1781267276c8cc5fd6774ee9733f680aee5
Collins Kiptoo <collinskandie1@gmail.com>
2023-04-16 22:02:48 +0300

    initial commits :smile:

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function login() {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+  
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value)) {
+      alert("Please enter a valid email address");
+    }
+      return;
+    }
+  
+    // Password validation
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-=_+[\]{}|\\;:'",.<>/?]).{8,}$/;
+    if (!passwordRegex.test(passwordInput.value)) {
+      alert("Please enter a valid password with at least 8 characters, including upper and lower case letters, digits, and special characters");
+      return;
+    }
+  
+    // If both inputs are valid, proceed with login logic
+    // ...
+
+  
